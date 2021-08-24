a5bb8e568555850d32002ef0a1d6663876a26817
duongnln96 <nguyenlengocduong@gmail.com>
2021-08-24 23:58:25 +0700

    GenData inject product data to psql

--- generate-data/main.py
+++ generate-data/main.py
@@ -1,9 +1,10 @@
-from os import name
 from typing import Dict, List
-import pandas
 import csv
 
 from pymongo import MongoClient
+
+import psycopg2
+
 from typing import (
     Dict,
 )
@@ -45,8 +46,43 @@ class SimpleMongoClient:
                 linecount += 1
                 self.insert_row(self.parser_data(data=line, id=linecount))
 
+class SimplePsqlClient:
+    def __init__(self, data_path: str):
+        self._conn = psycopg2.connect(host="localhost", port="5432",
+                                    database="product",
+                                    user="postgres",
+                                    password="postgres")
+        self._path = data_path
+
+    def insert_row(self, data: Product):
+        insertCmd = """INSERT INTO products (name, description, price, sku) VALUES (%s, %s, %s, %s);"""
+        with self._conn.cursor() as cursor:
+            cursor.execute(insertCmd, (data.name, data.description, data.price, data.sku))
+
+    def parser_data(self, data: List[str], id: int):
+        return Product(
+            _id=id,
+            name=data[0],
+            description=data[1],
+            sku=data[2],
+            price=data[3]
+        )
+
+    def write_data(self):
+        with open(self._path) as csv_file:
+            csv_reader = csv.reader(csv_file, )
+            _ = next(csv_reader)
+            linecount = 0
+            for line in csv_reader:
+                linecount += 1
+                self.insert_row(self.parser_data(data=line, id=linecount))
+
+        self._conn.commit()
+
 def main():
-    client = SimpleMongoClient("products", "./data/products.csv")
+    # client = SimpleMongoClient("products", "./data/products.csv")
+    # client.write_data()
+    client = SimplePsqlClient("./data/products.csv")
     client.write_data()
 
 if __name__ == "__main__":
