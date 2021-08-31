44120651a48992eb1f1229b570eebf4e136bc59a
duongnln96 <nguyenlengocduong@gmail.com>
2021-08-31 23:18:38 +0700

    Currency: gRPC Bi-directional streaming

--- currency/internal/data/currencydb.go
+++ currency/internal/data/currencydb.go
@@ -4,13 +4,16 @@ import (
 	"context"
 	"encoding/xml"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
 
 	"go.uber.org/zap"
 )
 
-type CurrencyRates interface {
+type CurrencyRatesI interface {
+	MonitorRate(time.Duration) chan struct{}
 	GetRate(string, string) (float64, error)
 }
 
@@ -25,7 +28,7 @@ type currencyRates struct {
 	rates map[string]float64
 }
 
-func NewCurrencyData(deps CurrencyRatesDeps) (CurrencyRates, error) {
+func NewCurrencyData(deps CurrencyRatesDeps) (CurrencyRatesI, error) {
 	cr := &currencyRates{
 		log:   deps.Log,
 		ctx:   deps.Ctx,
@@ -35,6 +38,47 @@ func NewCurrencyData(deps CurrencyRatesDeps) (CurrencyRates, error) {
 	err := cr.getRates()
 
 	return cr, err
+}
+
+// MonitorRates checks the rates in the ECB API every interval and sends a message to the
+// returned channel when there are changes
+//
+// Note: the ECB API only returns data once a day, this function only simulates the changes
+// in rates for demonstration purposes
+func (cd *currencyRates) MonitorRate(interval time.Duration) chan struct{} {
+	retChan := make(chan struct{})
+
+	go func() {
+		ticker := time.NewTicker(interval)
+		for {
+			select {
+			case <-ticker.C:
+				// just add a random difference to the rate and return it
+				// this simulates the fluctuations in currency rates
+				for k, v := range cd.rates {
+					// change can be 10% of original value
+					change := (rand.Float64() / 10)
+
+					// is this a postive or negative change
+					direction := rand.Intn(1)
+
+					if direction == 0 {
+						// new value with be min 90% of old
+						change = 1 - change
+					} else {
+						// new value will be 110% of old
+						change = 1 + change
+					}
+					// modify the rate
+					cd.rates[k] = v * change
+				}
+				// notify updates, this will block unless there is a listener on the other go routines
+				retChan <- struct{}{}
+			}
+		}
+	}()
+
+	return retChan
 }
 
 func (cd *currencyRates) GetRate(base string, destination string) (float64, error) {
--- currency/internal/server/currency.go
+++ currency/internal/server/currency.go
@@ -2,34 +2,63 @@ package server
 
 import (
 	"context"
+	"io"
+	"time"
 
 	"github.com/duongnln96/building-microservices-golang/currency/internal/data"
-	pb "github.com/duongnln96/building-microservices-golang/currency/protos/currency"
+	protos "github.com/duongnln96/building-microservices-golang/currency/protos/currency"
 	"go.uber.org/zap"
 )
 
 type CurrencyDeps struct {
 	Log *zap.SugaredLogger
 	Ctx context.Context
-	Db  data.CurrencyRates
+	Db  data.CurrencyRatesI
 }
 
 type currency struct {
-	pb.UnimplementedCurrencyServer
-	log *zap.SugaredLogger
-	ctx context.Context
-	db  data.CurrencyRates
+	protos.UnimplementedCurrencyServer
+	log           *zap.SugaredLogger
+	ctx           context.Context
+	db            data.CurrencyRatesI
+	subscriptions map[protos.Currency_SubscribeRatesServer][]*protos.RateRequest
 }
 
-func NewCurrency(deps CurrencyDeps) pb.CurrencyServer {
-	return &currency{
-		log: deps.Log,
-		ctx: deps.Ctx,
-		db:  deps.Db,
+func NewCurrency(deps CurrencyDeps) protos.CurrencyServer {
+	c := currency{
+		log:           deps.Log,
+		ctx:           deps.Ctx,
+		db:            deps.Db,
+		subscriptions: make(map[protos.Currency_SubscribeRatesServer][]*protos.RateRequest),
+	}
+
+	go c.handleUpdate()
+
+	return &c
+}
+
+func (c *currency) handleUpdate() {
+	rateUpdateChan := c.db.MonitorRate(10 * time.Second)
+	for range rateUpdateChan {
+		c.log.Info("Get update rates")
+
+		for k, v := range c.subscriptions {
+			for _, rateReq := range v {
+				rate, err := c.db.GetRate(rateReq.GetBase().String(), rateReq.Destination.String())
+				if err != nil {
+					c.log.Error("Unable to get update rate", " base ", rateReq.GetBase().String(), " destination ", rateReq.GetDestination().String())
+				}
+
+				err = k.Send(&protos.RateResponse{Base: rateReq.Base, Destination: rateReq.Destination, Rate: rate})
+				if err != nil {
+					c.log.Error("Unable to send updated rate, ", err)
+				}
+			}
+		}
 	}
 }
 
-func (c *currency) GetRate(ctx context.Context, r *pb.RateRequest) (*pb.RateResponse, error) {
+func (c *currency) GetRate(ctx context.Context, r *protos.RateRequest) (*protos.RateResponse, error) {
 	c.log.Infof("Handle request for GetRate base %s - dest %s", r.GetBase(), r.GetDestination())
 
 	rate, err := c.db.GetRate(r.GetBase().String(), r.GetDestination().String())
@@ -37,5 +66,31 @@ func (c *currency) GetRate(ctx context.Context, r *pb.RateRequest) (*pb.RateResp
 		return nil, err
 	}
 
-	return &pb.RateResponse{Rate: rate}, nil
+	return &protos.RateResponse{Base: r.Base, Destination: r.Destination, Rate: rate}, nil
+}
+
+func (c *currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
+	for {
+		rr, err := src.Recv()
+		if err == io.EOF {
+			c.log.Infof("Client has closed the connection %+v", err)
+			break
+		}
+
+		if err != nil {
+			c.log.Error("Unable to read from client ", err)
+			return err
+		}
+
+		c.log.Info("Hanle client request: ", "{ base: ", rr.GetBase(), ", destination: ", rr.GetDestination(), " }")
+
+		rrs, ok := c.subscriptions[src]
+		if !ok {
+			c.log.Debug("Subscriber is not ok")
+			rrs = []*protos.RateRequest{}
+		}
+		rrs = append(rrs, rr)
+		c.subscriptions[src] = rrs
+	}
+	return nil
 }
